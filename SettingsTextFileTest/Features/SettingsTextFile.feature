b738aa68fa8eef47b9e390787c7da11d0a388ea9
impolitetulip <87053379+impolitetulip@users.noreply.github.com>
2021-11-12 21:01:40 -0600

    Added first automated test, dotnetcore workflow

---
+++ SettingsTextFileTest/Features/SettingsTextFile.feature
@@ -0,0 +1,10 @@
+﻿Feature: Settings Text File
+
+Background:
+	Given I create a new settings file
+
+Scenario: Read/Write String
+	When I add the string 'UserWorkstation' with value 'DESKTOP-123' to the settings file
+	And I write the settings file
+	And I read the settings file
+	Then the string 'UserWorkstation' with value 'DESKTOP-123' is in the settings file
---
+++ SettingsTextFileTest/Steps/SettingsTextFileStepDefinitions.cs
@@ -0,0 +1,76 @@
+﻿using TechTalk.SpecFlow;
+using SettingsFile;
+using NUnit.Framework;
+
+namespace SettingsTextFileTest.Steps
+{
+    [Binding]
+    public sealed class SettingsTextFileStepDefinitions
+    {
+        private SettingsTextFile settingsTextFile;
+
+        public SettingsTextFileStepDefinitions(ScenarioContext scenarioContext)
+        {
+
+        }
+
+        #region ScenarioSteps
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            settingsTextFile = null;
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+
+        }
+
+        #endregion
+
+        #region GivenSteps
+
+        [Given(@"I create a new settings file")]
+        public void GivenICreateANewSettingsFile()
+        {
+            settingsTextFile = new SettingsTextFile();
+        }
+
+        #endregion
+
+        #region WhenSteps
+
+        [When(@"I write the settings file")]
+        public void WhenIWriteTheSettingsFile()
+        {
+            settingsTextFile.Write("settings.txt");
+        }
+
+        [When(@"I read the settings file")]
+        public void WhenIReadTheSettingsFile()
+        {
+            settingsTextFile = SettingsTextFile.Read("settings.txt");
+        }
+
+        [When(@"I add the string '(.*)' with value '(.*)' to the settings file")]
+        public void WhenIAddTheStringWithValueToTheSettingsFile(string key, string value)
+        {
+            settingsTextFile.Add(key, value);
+        }
+
+        #endregion
+
+        #region ThenSteps
+
+        [Then(@"the string '(.*)' with value '(.*)' is in the settings file")]
+        public void ThenTheStringWithValueIsInTheSettingsFile(string key, string value)
+        {
+            Assert.True(settingsTextFile.Strings.ContainsKey(key));
+            Assert.AreEqual(value, settingsTextFile.Strings[key]);
+        }
+
+        #endregion
+    }
+}
